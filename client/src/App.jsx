4fc7a2ca72c2f6c942587ab0f0676cc84b43928c
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-11-04 02:24:06 +0300

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Routes , Route} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Blog from "./pages/Blog.jsx";
+
+const App = () => {
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/blog" element={<Blog />} />
+            </Routes>
+        </div>
+    );
+};
+export default App;

fc2b8dcab2adae0c0a15e0e677846fb54ef1962a
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-11-05 18:41:22 +0300

    BlogCard, BlogList.jsx

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -1,24 +1,38 @@
-import React, { useState } from 'react'
-import { blog_data, blogCategories } from '../assets/assets'
+import {blog_data, blogCategories} from '../assets/assets'
+import {useState} from "react";
+import {motion} from "motion/react";
+import blog from "../pages/Blog.jsx";
+import BlogCard from "./BlogCard.jsx";
 
 const BlogList = () => {
-    const [menu, setMenu] = useState('All')
+
+    const [menu, setMenu] = useState("All")
 
     return (
         <div>
             <div className='flex justify-center gap-4 sm:gap-8 my-10 relative'>
                 {blogCategories.map((item) => (
                     <div key={item} className='relative'>
-
+                        <button
+                            onClick={() => setMenu(item)}
+                            className={`cursor-pointer text-gray-500 
+                            ${menu === item && "text-white px-4 mt-0.5"}`}>
+                            {item}
+                            {menu === item && (
+                                <motion.div layoutId="underline"
+                                            transition={{type: "spring", stiffness: 500, damping: 30}}
+                                            className="absolute left-0 right-0 top-0 h-7 -z-1 bg-primary rounded-full">
+                                </motion.div>)}
+                        </button>
                     </div>
                 ))}
             </div>
-            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mb-24 mx-8 sm:mx-16 xl:mx-40'>
-                {/* ---blog cards------ */}
+            <div
+                className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mb-24 mx-8 sm:mx-16 xl:mx-40'>
+                {blog_data.filter((blog) => menu === "All" ? true : blog.category === menu).map((blog) => <BlogCard
+                    key={blog._id} blog={blog}/>)}
             </div>
         </div>
-
-
-
     );
 };
+export default BlogList;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { assets } from "../assets/assets.js";
-import { useNavigate } from "react-router-dom";
+import {assets} from "../assets/assets.js";
+import {useNavigate} from "react-router-dom";
 
 const Navbar = () => {
     const navigate = useNavigate();
@@ -13,13 +13,12 @@ const Navbar = () => {
                 alt="logo"
                 className="w-32 sm:w-44 cursor-pointer"
             />
-
             <button
-                // onClick={() => navigate('/admin')}
-                className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5"
-            >
-                {/* {token ? 'Dashboard' : 'Login'} */}
-                <img src={assets.arrow} className="w-3" alt="arrow" />
+                onClick={() => navigate('/admin')}
+                className="flex items-center gap-2 rounded-full
+                text-sm bg-primary text-white px-10 py-2.5 cursor-pointer">
+                Login
+                <img src={assets.arrow} className="w-3" alt="arrow"/>
             </button>
         </div>
     );

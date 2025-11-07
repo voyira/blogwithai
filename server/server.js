1c58716a59fcbbbcc3fcfe33ef744900870a3138
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-11-08 01:06:41 +0300

    server.js added

---
+++ server/server.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import 'dotenv/config'
+import cors from 'cors'
+// import connectDB from './configs/db.js';
+// import adminRouter from './routes/adminRoutes.js';
+// import blogRouter from './routes/blogRoutes.js';
+
+const app = express();
+
+await  connectDB()
+
+app.use(cors())
+app.use(express.json())
+
+//Routes
+app.get('/', (req, res)=>{
+    res.send("api working bro");
+})
+
+app.use('/api/admin', adminRouter)
+app.use('/api/blog', blogRouter)
+
+
+const PORT =process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log("server is running on PORT"+ PORT);
+})
+
+export default app;

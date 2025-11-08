b4ec844ad3609f6742c3beaddf5e1e7eb818c030
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-11-08 19:24:33 +0300

    Routes added

---
+++ server/routes/adminRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import { adminLogin, approveCommentById, deleteCommentById, getAllBlogsAdmin, getALlComments, getDashboard } from '../controllers/adminController.js';
+import auth from '../middleware/auth.js';
+const adminRouter = express.Router();
+
+adminRouter.post('/login', adminLogin);
+adminRouter.get('/comments',auth, getALlComments);
+adminRouter.get('/blogs',auth, getAllBlogsAdmin);
+adminRouter.post('/delete-comment',auth, deleteCommentById);
+adminRouter.post('/approve-comment',auth, approveCommentById);
+adminRouter.get('/dashboard',auth, getDashboard);
+
+export default adminRouter;
